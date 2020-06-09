7c5b81e98d185077fda0f2d7ffbfc2771bfc3d8c
Adrian Mowat <adrian.mowat@gmail.com>
2020-06-09 21:49:47 +0100

    Third go - using structs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,59 @@
-// "twelve drummers drumming"
-// "eleven pipers piping"
-// "ten lords a-leaping"
-// "nine ladies dancing"
-// "eight maids a-milking"
-// "seven wans a-swimming"
-// "six geese a-laying"
-// "five gold rings"
-// "four calling birds"
-// "three French hens"
-// "two turtle doves"
-// "a partridge in a pear tree"
+struct Day {
+    num: usize,
+    gift: String,
+}
 
-fn main() {}
+impl Day {
+    fn new(num: usize, gift: String) -> Day {
+        Day { num, gift }
+    }
+
+    fn lyric(&self) -> String {
+        format!(
+            "On the {} day of Christmas, my true love gave to me...",
+            nth(&self.num)
+        )
+    }
+}
+
+fn nth(n: &usize) -> String {
+    let suffix = match &n {
+        1 => String::from("st"),
+        2 => String::from("nd"),
+        3 => String::from("rd"),
+        _ => String::from("th"),
+    };
+    format!("{}{}", n, suffix)
+}
+
+fn print_song(days: [Day; 12]) {
+    for n in 1..=12 {
+        for (i, day) in days.iter().skip(12 - n).enumerate() {
+            if i == 0 {
+                println!("\n{}", day.lyric());
+            }
+            if day.num == 1 && n != 1 {
+                println!("  and {}", day.gift);
+            } else {
+                println!("  {}", day.gift);
+            }
+        }
+    }
+}
+
+fn main() {
+    print_song([
+        Day::new(12, String::from("twelve drummers drumming")),
+        Day::new(11, String::from("eleven pipers piping")),
+        Day::new(10, String::from("ten lords a-leaping")),
+        Day::new(9, String::from("nine ladies dancing")),
+        Day::new(8, String::from("eight maids a-milking")),
+        Day::new(7, String::from("seven swans a-swimming")),
+        Day::new(6, String::from("six geese a-laying")),
+        Day::new(5, String::from("five gold rings")),
+        Day::new(4, String::from("four calling birds")),
+        Day::new(3, String::from("three French hens")),
+        Day::new(2, String::from("two turtle doves")),
+        Day::new(1, String::from("a partridge in a pear tree")),
+    ]);
+}
